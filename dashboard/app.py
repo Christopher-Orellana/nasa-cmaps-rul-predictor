a28bc3efaca0ab4791158a44345b322d508cb9e7
CHRIS-1A <christopherorellanave@gmail.com>
2026-03-11 02:10:58 -0400

    Initialize streamlit dashboard base

---
+++ dashboard/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Set basic dashboard page settings
+st.set_page_config(page_title="NASA CMAPSS Dashboard", layout="wide")
+st.title("Predictive Maintenance Decision Dashboard")
+st.write("Streamlit is working.")
